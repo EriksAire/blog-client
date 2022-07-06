fc98083bec7c0ad802e7b35833e24a67657413cd
DESKTOP-8LFJ8H8\Nox <eriks.aire.07@gmail.com>
2022-07-06 16:04:58 +0300

    Added constants and types files

--- constants.ts
+++ constants.ts
@@ -1,1 +1,2 @@
-export const API_URL = 'https://localhost:7222/api/HomeApi';
+export const API_URL = "https://localhost:7222/api/HomeApi"
+export const API_URL_AUTH = "https://localhost:7222/api"
---
+++ types/post.ts
@@ -0,0 +1,8 @@
+type PostType = {
+	id: number
+	title: string
+	postBody: string
+	rating: number
+}
+
+export default PostType
